89f4968b88b32e06b60d364886a998bbd83c540d
Jordan Kaye <jordan.kaye2@gmail.com>
2024-12-03 07:09:56 -0700

    Day 3: single-pass sweep

--- src/day3.rs
+++ src/day3.rs
@@ -13,65 +13,22 @@ pub fn uncorrupt(input: &str) -> u64 {
 
 #[aoc(day3, part2)]
 pub fn uncorrupt_conditional(input: &str) -> u64 {
-    let mul_re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let do_re = Regex::new(r"do\(\)").unwrap();
-    let dont_re = Regex::new(r"don't\(\)").unwrap();
+    let re = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)|(?<do>do\(\))|(?<dont>don't\(\))").unwrap();
 
-    let mut muls = mul_re
-        .captures_iter(input)
+    let mut toggle = 1;
+    re.captures_iter(input)
         .map(|c| {
-            let (_, [a, b]) = c.extract();
-            (
-                c.get(0).unwrap().start(),
-                a.parse::<u64>().unwrap() * b.parse::<u64>().unwrap(),
-            )
-        })
-        .peekable();
-    let mut dos = do_re
-        .captures_iter(input)
-        .map(|c| c.get(0).unwrap().start())
-        .peekable();
-    let mut donts = dont_re
-        .captures_iter(input)
-        .map(|c| c.get(0).unwrap().start())
-        .peekable();
-
-    let mut dont = donts.next().unwrap();
-
-    let mut total = 0;
-    while muls.peek().is_some() {
-        while let Some((idx, val)) = muls.next()
-            && idx < dont
-        {
-            total += val;
-        }
-        while let Some(idx) = dos.peek()
-            && *idx < dont
-        {
-            dos.next();
-        }
-
-        if let Some(do_) = dos.peek() {
-            while let Some((idx, _)) = muls.peek()
-                && idx < do_
-            {
-                muls.next();
-            }
-            while let Some(idx) = donts.peek()
-                && *idx < *do_
-            {
-                donts.next();
-            }
-            if let Some(dont_) = donts.peek() {
-                dont = *dont_;
+            if c.name("do").is_some() {
+                toggle = 1;
+                0
+            } else if c.name("dont").is_some() {
+                toggle = 0;
+                0
             } else {
-                dont = usize::MAX;
+                let a = c.name("a").unwrap().as_str();
+                let b = c.name("b").unwrap().as_str();
+                a.parse::<u64>().unwrap() * b.parse::<u64>().unwrap() * toggle
             }
-            donts.next();
-        } else {
-            break;
-        }
-        dos.next();
-    }
-    total
+        })
+        .sum()
 }
